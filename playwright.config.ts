a7daafe7bda4763b42ab48554b436c707a4156db
tharuni-bectran <tharuni.pulipati@bectran.com>
2025-08-08 23:11:29 -0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  reporter: [['list'], ['html']], // html report + console list
   use: {
-    baseURL: process.env.APP_BASE_URL || 'https://pilot-b2bcredit.bectran.com/b2bcredit/',
-    trace: 'on-first-retry',
-    video: 'retain-on-failure'
-  }
+    baseURL: process.env.APP_BASE_URL,
+    screenshot: 'only-on-failure',     // 'on', 'only-on-failure', or 'off'
+    video: 'retain-on-failure',        // 'on', 'retain-on-failure', 'off'
+    trace: 'on-first-retry',           // 'on', 'off', 'retain-on-failure', etc.
+  },
+  // Optional: where raw artifacts go (defaults to ./test-results)
+  // outputDir: 'test-results',
 });
